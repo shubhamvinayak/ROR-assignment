790f116a9937097f8028f5a92b53b49376c68570
V <SV067337@cerner.net>
2020-10-30 21:41:01 +0530

    proper formatting

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -11,11 +11,11 @@ module Api
     users = JSON.parse(Net::HTTP.get(URI(@base_url + '/users/')))
     comments = JSON.parse(Net::HTTP.get(URI(@base_url + '/comments/')))
     output_array = []
+
     posts.each do |post|
       postId = post['id']
       postName = post['title']
       postContent = post['body']
-
       temp_hash = {
         "id": postId,
         "name": postName,
@@ -24,14 +24,14 @@ module Api
         "authorContact": '',
         "comments": []
       }
-      users.each do |user|
-        temp_hash['authorName'] = user['username']
-        temp_hash['authorContact'] = user['email']
-      end
 
-      comments.each do |comment|
-        next unless comment['postId'] == postId
+    users.each do |user|
+      temp_hash['authorName'] = user['username']
+      temp_hash['authorContact'] = user['email']
+     end
 
+    comments.each do |comment|
+      next unless comment['postId'] == postId
         comment_hash = {
           "id": comment['id'],
           "name": comment['name'],
@@ -39,8 +39,8 @@ module Api
           "contact": comment['email']
         }
         temp_hash[:comments] << comment_hash
-      end
-      output_array << temp_hash
+    end
+        output_array << temp_hash
     end
     output_array
   end
